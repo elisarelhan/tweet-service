a6629cf89109ca68b3a713207592f69b3a0d5485
elisarelhan <79890999+elisarelhan@users.noreply.github.com>
2022-12-11 23:17:35 +0530

    Update TweetRepo.java

--- src/main/java/com/tweetapp/tweets/repository/TweetRepo.java
+++ src/main/java/com/tweetapp/tweets/repository/TweetRepo.java
@@ -14,7 +14,7 @@ import com.tweetapp.tweets.entity.Tweet;
 public interface TweetRepo  extends JpaRepository<Tweet, Integer> {
     List<Tweet> findByUserEmailOrderByUpdatedDateDesc(String userEmail);
     @Transactional
-    @Query(value="SELECT * FROM tweetdb.tweet_details INNER JOIN authdb.users where users.email= tweet_details.user_email ORDER BY updated_date DESC;",nativeQuery=true) 
+    @Query(value="SELECT * FROM tweet_details ORDER BY updated_date DESC;",nativeQuery=true) 
     List<Tweet> findAllTweetsOrderByUpdatedDateDesc();
      Tweet findByTweetIdOrderByUpdatedDateDesc(int tweetId);
      
